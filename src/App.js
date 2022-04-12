4d82204b8584ec134f63f0462015a580cfd7e1e2
unknown <habibighofrane0000@gmail.com>
2022-04-12 03:28:20 -1100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React</a>
       </header>
     </div>
   );
